f29a357174674d07644b363bd4779e3afc0a034b
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2019-04-02 15:22:01 +0300

    Project setup

---
+++ FAPEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FAPEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
